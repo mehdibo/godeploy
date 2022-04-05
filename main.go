7b8bf5351718e9cca583921a9fbdbdc0d6a90bc7
Mehdi Bounya <mehdi.bounya@gmail.com>
2022-04-05 00:45:15 +0000

    Load .env files in main.go

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
-package go_deploy
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
+var (
+	appEnv string
+)
+
+func loadDotEnvFiles() {
+	/*
+		We load the .env files in the following order:
+		.env
+		.env.local
+		.env.$APP_ENV
+		.env.$APP_ENV.local
+	*/
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	appEnv = os.Getenv("APP_ENV")
+	if appEnv == "" {
+		appEnv = "dev"
+	}
+	files := []string{
+		".env.local",
+		".env." + appEnv + "",
+		".env." + appEnv + ".local",
+	}
+	for _, file := range files {
+		_ = godotenv.Overload(file)
+	}
+}
+
+func main() {
+	loadDotEnvFiles()
+}
