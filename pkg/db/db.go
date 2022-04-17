ea4fd710b9d129644f119ccacc657c0a1ae9b104
Mehdi Bounya <mehdi.bounya@gmail.com>
2022-04-17 06:22:10 +0000

    Silence gorm logs

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -4,10 +4,13 @@ import (
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewDb(dsn string) (db *gorm.DB, err error) {
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 }
 
 func AutoMigrate(db *gorm.DB) error {
