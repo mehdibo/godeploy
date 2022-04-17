06619689086c3fbe7e7571d0d5d85851934d2e8d
Mehdi Bounya <mehdi.bounya@gmail.com>
2022-04-17 04:04:39 +0000

    Add ValidateBasicAuth to server pkg

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"crypto/subtle"
 	"github.com/labstack/echo/v4"
+	"github.com/mehdibo/go_deploy/pkg/auth"
+	"github.com/mehdibo/go_deploy/pkg/db"
 	"gorm.io/gorm"
 	"net/http"
+	"time"
 )
 
 // Server Represents a server to handle requests, must implement GoDeploy.ServerInterface
@@ -17,8 +21,26 @@ func NewServer(db *gorm.DB) *Server {
 }
 
 // Ping returns a simple JSON payload to test the server
-func (srv Server) Ping(ctx echo.Context) error {
+func (srv *Server) Ping(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, map[string]string{
 		"message": "pong",
 	})
+}
+
+// ValidateBasicAuth validate basic auth credentials, used with built-in middleware
+func (srv *Server) ValidateBasicAuth(username string, rawToken string, ctx echo.Context) (bool, error) {
+	var user db.User
+	// Get user
+	tx := srv.db.First(&user, "username = ?", username)
+	if tx.RowsAffected != 1 {
+		return false, nil
+	}
+	// Verify token
+	providedToken := auth.HashToken(rawToken)
+	if subtle.ConstantTimeCompare([]byte(providedToken), []byte(user.HashedToken)) == 1 {
+		srv.db.Model(&user).Update("LastUsedAt", time.Now())
+		ctx.Set(auth.UserKey, user)
+		return true, nil
+	}
+	return false, nil
 }
